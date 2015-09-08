914d368d24628ead45c8240988d2e5207e23870e
Powpow Shen <powpowfp@gmail.com>
2015-09-08 11:32:06 +0000

    initialization

---
+++ app.js
@@ -0,0 +1,27 @@
+var parser = osmread.parse({
+  filePath: 'path/to/osm.xml',
+    endDocument: function(){
+      console.log('document end');
+    },
+    bounds: function(bounds){
+      console.log('bounds: ' + JSON.stringify(bounds));
+    },
+    node: function(node){
+      console.log('node: ' + JSON.stringify(node));
+    },
+    way: function(way){
+      console.log('way: ' + JSON.stringify(way));
+    },
+    relation: function(relation){
+      console.log('relation: ' + JSON.stringify(relation));
+    },
+    error: function(msg){
+      console.log('error: ' + msg);
+    }
+});
+
+// you can pause the parser
+// parser.pause();
+//
+// // and resume it again
+// parser.resume();
